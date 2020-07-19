b8af23f329e4da8692f3d13e94fe5d9aeab2ebbd
GuGaYoung <rkdudda12@naver.com>
2020-07-19 15:40:08 +0900

    버튼들 누르면 음악 나오도록 하기

--- app/src/main/java/com/app/hackathon/MainActivity.java
+++ app/src/main/java/com/app/hackathon/MainActivity.java
@@ -1,15 +1,120 @@
 package com.app.hackathon;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button oneBtn, twoBtn, threeBtn, fourBtn, fiveBtn, sixBtn, sevenBtn, eightBtn, nineBtn, tenBtn, elevenBtn, twelveBtn;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        oneBtn = findViewById(R.id.one);
+        twoBtn = findViewById(R.id.two);
+        threeBtn = findViewById(R.id.three);
+        fourBtn = findViewById(R.id.four);
+        fiveBtn = findViewById(R.id.five);
+        sixBtn = findViewById(R.id.six);
+        sevenBtn = findViewById(R.id.seven);
+        eightBtn = findViewById(R.id.eight);
+        nineBtn = findViewById(R.id.nine);
+        tenBtn = findViewById(R.id.ten);
+        elevenBtn = findViewById(R.id.eleven);
+        twelveBtn = findViewById(R.id.twelve);
+
+        MySoundPlayer.initSounds(getApplicationContext());
+
+        oneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MySoundPlayer.play(MySoundPlayer.HAEGEUM);
+            }
+        });
+
+        twoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MySoundPlayer.play(MySoundPlayer.KKWAENGGWARI);
+            }
+        });
+
+        threeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MySoundPlayer.play(MySoundPlayer.PIPE);
+            }
+        });
+
+        fourBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MySoundPlayer.play(MySoundPlayer.KOREADRUM);
+            }
+        });
+
+        fiveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MySoundPlayer.play(MySoundPlayer.AJAENG);
+            }
+        });
+
+        sixBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MySoundPlayer.play(MySoundPlayer.GAYAGEUM);
+            }
+        });
+
+        sevenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MySoundPlayer.play(MySoundPlayer.HMM);
+            }
+        });
+
+        eightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MySoundPlayer.play(MySoundPlayer.JANGGU);
+            }
+        });
+
+        nineBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MySoundPlayer.play(MySoundPlayer.SONG);
+            }
+        });
+
+        tenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MySoundPlayer.play(MySoundPlayer.TRANSFERPIPE);
+            }
+        });
+
+        elevenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MySoundPlayer.play(MySoundPlayer.TRANSFERSTATION);
+            }
+        });
+
+        twelveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MySoundPlayer.play(MySoundPlayer.HEUNG);
+            }
+        });
     }
 }
